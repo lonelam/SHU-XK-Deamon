6edc0c2b3f21c42eef48e74b94d5488efda5a570
Lonelam <laizenan@gmail.com>
2017-02-26 11:39:22 +0800

    whatever

--- spawn_class.py
+++ spawn_class.py
@@ -5,16 +5,19 @@ import cookielib
 import re
 import time
 import webbrowser
+import os
+import os.path
 import zlib
-from Auto_CHPTCHA import *
+
+#from Auto_CHPTCHA import *
 from pass_input import *
 class sim_client:
     def __init__(self, username, password, model_dict):
         self.model_dict = model_dict
         self.TxtUsername = username
         self.TxtPassword = password
-        self.IndexUrl = 'http://xk.autoisp.shu.edu.cn/'
-        self.CodeUrl = 'http://xk.autoisp.shu.edu.cn/Login/GetValidateCode?%20%20+%20GetTimestamp()'
+        self.IndexUrl = 'http://xk.autoisp.shu.edu.cn:8080/'
+        self.CodeUrl = 'http://xk.autoisp.shu.edu.cn:8080/Login/GetValidateCode?%20%20+%20GetTimestamp()'
         self.IndexBody = {'txtUserName':self.TxtUsername,'txtPassword':self.TxtPassword}
         self.cookie = cookielib.LWPCookieJar()
         self.cookieHandler = urllib2.HTTPCookieProcessor(self.cookie)
@@ -24,7 +27,8 @@ class sim_client:
         temp_file = open('temp_code.jpg', 'wb')
         temp_file.write(self.opener.open(self.CodeUrl).read())
         temp_file.close()
-        validate_code = Auto_CHPTCHA('temp_code.jpg',model_dict)
+   #     validate_code = Auto_CHPTCHA('temp_code.jpg',model_dict)
+        validate_code = raw_input('manually type in the validate code you see\n')
         self.IndexBody['txtValiCode'] = str(validate_code)
         content = self.__login()
         return content
@@ -44,8 +48,8 @@ def client_login(username, password , model_dcit):
     return client
 #for course inquire, the parameter is class name. for course selection,it's the list of class
 def request_constructor(username, request_type, parameter):
-    query_url = 'http://xk.autoisp.shu.edu.cn/StudentQuery/CtrlViewQueryCourse'
-    select_url = 'http://xk.autoisp.shu.edu.cn/CourseSelectionStudent/CtrlViewOperationResult'
+    query_url = 'http://xk.autoisp.shu.edu.cn:8080/StudentQuery/CtrlViewQueryCourse'
+    select_url = 'http://xk.autoisp.shu.edu.cn:8080/CourseSelectionStudent/CtrlViewOperationResult'
     if request_type == 'query_course':
         raw_data_sheet = 'CourseName=%' + urllib.quote(parameter) + '&'
         access_url = query_url
@@ -59,7 +63,7 @@ def request_constructor(username, request_type, parameter):
 def course_attack(username, password, model_dcit, class_list, idle_time = 2, reset_time = 50):
     client = client_login(username, password, model_dict)
     request = request_constructor(username, 'select_course', class_list)
-    vain = client.opener.open('http://xk.autoisp.shu.edu.cn/CourseSelectionStudent/FastInput')
+    vain = client.opener.open('http://xk.autoisp.shu.edu.cn:8080/CourseSelectionStudent/FastInput')
     flag = True
     embark = time.time()
     while flag:
@@ -69,33 +73,33 @@ def course_attack(username, password, model_dcit, class_list, idle_time = 2, res
             print 'check'
             flag = True
         if len(re.findall('已选此课程', reponse)) != 0:
-            vain = client.opener.open('http://xk.autoisp.shu.edu.cn/CourseSelectionStudent/FastInput')
+            vain = client.opener.open('http://xk.autoisp.shu.edu.cn:8080/CourseSelectionStudent/FastInput')
         if len(re.findall('限制', reponse)) != 0:
             print 'hypocritical'
             return None 
         if time.time() - embark > reset_time:
             print 'farewell'
             client = client_login(username, password, model_dict)
-            vain = client.opener.open('http://xk.autoisp.shu.edu.cn/CourseSelectionStudent/FastInput')
+            vain = client.opener.open('http://xk.autoisp.shu.edu.cn:8080/CourseSelectionStudent/FastInput')
             embark = time.time()
         time.sleep(idle_time)
     print 'peace'
     return None
 
-def wise_course_attack(username, password, model_dcit, class_list, idle_time = 2, reset_time = 100):
+def wise_course_attack(username, password, model_dcit, class_list, idle_time = 2, reset_time = 5000):
     client = client_login(username, password, model_dict)
     request = request_constructor(username, 'select_course', class_list)
-    vain = client.opener.open('http://xk.autoisp.shu.edu.cn/CourseSelectionStudent/FastInput')
+    vain = client.opener.open('http://xk.autoisp.shu.edu.cn:8080/CourseSelectionStudent/FastInput')
     flag = True
     embark = time.time()
     while flag:
         #print client.opener.open(request).read()
-        client.opener.open('http://xk.autoisp.shu.edu.cn/Login/Logout')
-        vain = client.opener.open('http://xk.autoisp.shu.edu.cn/Login/Index')
+        client.opener.open('http://xk.autoisp.shu.edu.cn:8080/Login/Logout')
+        vain = client.opener.open('http://xk.autoisp.shu.edu.cn:8080/Login/Index')
         client = client_login(username, password, model_dict)
-        if client.opener.open('http://xk.autoisp.shu.edu.cn/StudentQuery/QueryEnrollRank').read().find('排名') != -1:
+        if client.opener.open('http://xk.autoisp.shu.edu.cn:8080/StudentQuery/QueryEnrollRank').read().find('排名') != -1:
             print time.time()
-        vain = client.opener.open('http://xk.autoisp.shu.edu.cn/CourseSelectionStudent/FastInput')
+        vain = client.opener.open('http://xk.autoisp.shu.edu.cn:8080/CourseSelectionStudent/FastInput')
         #print vain.read()
     print 'peace'
     return None
@@ -103,17 +107,17 @@ def wise_course_attack(username, password, model_dcit, class_list, idle_time = 2
 test_list = [{'classid' : '00874008', 'teacherid' : '1018'}]
 
 #preload svm model
-model_dir = os.getcwd() + '\\model'
-print model_dir
-files = os.listdir(model_dir)
-model_dict = {}
-for file in files:
-    temp_model = svm_load_model(model_dir + '\\' + file)
-    model_dict[str(file).strip('.model')] = temp_model
+#model_dir = os.getcwd() + '\\model'
+#print model_dir
+#files = os.listdir(model_dir)
+#model_dict = {}
+#for file in files:
+#    temp_model = svm_load_model(model_dir + '\\' + file)
+#    model_dict[str(file).strip('.model')] = temp_model
 
 #get input
 username = raw_input('enter student id:')
-password = pwd_input('enter passphrase:')
+password = pwd_input('enter passphrase(psw):')
 class_number = input('enter the number of class:')
 class_list = [{'classid' : '', 'teacherid' : ''} for i in range(class_number)]
 
@@ -121,4 +125,6 @@ for i in range(class_number):
     class_list[i]['classid'] = raw_input('the id of class ' +str(i) + ':')
     class_list[i]['teacherid'] = raw_input('the id of teacher ' +str(i) + ':')
 class_list = test_list
+
+model_dict = {}
 course_attack(username, password, model_dict, class_list)
